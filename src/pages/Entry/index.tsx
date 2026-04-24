13cb508e5178ace18aebc0d67255ec83661d9892
kwangsun <eyeyel@naver.com>
2026-04-24 14:27:27 +0900

    refactor: 공통컬러 변수명 변경 & 타이틀 이미지 추가

--- src/pages/Entry/index.tsx
+++ src/pages/Entry/index.tsx
@@ -13,35 +13,12 @@ export default function Entry() {
   const { addToastMessage } = useViewStore((s) => s.actions);
   const navigate = useNavigate();
 
-  // const { refetch, isFetching } = useUsersQuery();
   useMyCatsQuery();
 
   return (
     <main className={cn("Entry")}>
-      <div className={cn("background-sunset")}>
-        <div className={cn("sky")}>
-          {/* 태양 뒤쪽 구름 */}
-          <div className={cn("cloud", "large")} />
-          <div className={cn("cloud", "small")} />
-
-          <div className={cn("sun-wrapper")}>
-            <div className={cn("heat-haze")} />
-            <div className={cn("sun")}>
-              <div className={cn("sun-core")} />
-              <div className={cn("sun-flame")} />
-            </div>
-          </div>
-
-          {/* 태양 앞쪽 구름 (더 많이 가림) */}
-          <div className={cn("cloud", "large", "front")} />
-          <div className={cn("cloud", "medium", "front", "pos-1")} />
-          <div className={cn("cloud", "medium", "front", "pos-2")} />
-          <div className={cn("cloud", "small", "front")} />
-        </div>
-      </div>
-
       <div className={cn("content")}>
-        <h1 className={cn("title")}>CAT GANG</h1>
+        <h1 className={cn("title")}>냥만시대</h1>
         <div className={cn("menu")}>
           <Button
             className={cn("start")}
@@ -59,18 +36,6 @@ export default function Entry() {
           >
             랭킹
           </Button>
-          {/* <Button
-            disabled={isFetching}
-            onClick={async () => {
-              const { data } = await refetch();
-
-              addToastMessage({
-                message: `name : ${data?.name}`,
-              });
-            }}
-          >
-            유저정보
-          </Button> */}
         </div>
       </div>
     </main>
