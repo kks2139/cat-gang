85597c3e6d5233bf0385a9f07eb6cd213d07584c
kwangsun <eyeyel@naver.com>
2026-04-26 05:11:30 +0900

    chore: toss sdk 설치

---
+++ granite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "@apps-in-toss/web-framework/config";
+
+export default defineConfig({
+  appName: "cat-gang",
+  brand: {
+    displayName: "cat-gang", // 화면에 노출될 앱의 한글 이름으로 바꿔주세요.
+    primaryColor: "#3182F6", // 화면에 노출될 앱의 기본 색상으로 바꿔주세요.
+    icon: "", // 화면에 노출될 앱의 아이콘 이미지 주소로 바꿔주세요.
+  },
+  web: {
+    host: "192.168.45.226",
+    port: 5173,
+    commands: {
+      dev: "vite --host",
+      build: "tsc -b && vite build",
+    },
+  },
+  permissions: [],
+  outdir: "dist",
+});
